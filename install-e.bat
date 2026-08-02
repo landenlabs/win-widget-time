eca8af3ec76b2c66c33e2af6bac07f0fcdc8a1de
unknown <landenlabs@gmail.com>
2026-08-01 23:02:14 -0400

    add installer to e drive

--- install-e.bat
+++ install-e.bat
@@ -1,14 +1,15 @@
 @echo off
 
 echo Build and install WinWidgetTime
+set dstDir=e:\opt\bin
 
 :: Kill widget is running, so it can be re-built
 taskkill /IM WinWidgetTime.exe /F 2>nul
 dotnet publish WinWidgetTime.csproj -c Release -r win-x64 --self-contained false -p:PublishSingleFile=true
 
 :: Make directory to hold the exe and assets
-mkdir c:\opt\bin\winwidgets 2>nul
-xcopy /E /Y bin\Release\net8.0-windows\win-x64\publish\* c:\opt\bin\winwidgets\
+mkdir %dstDir%\winwidgets 2>nul
+xcopy /E /Y bin\Release\net8.0-windows\win-x64\publish\* %dstDir%\winwidgets\
 
-echo start "" c:\opt\bin\winwidgets\WinWidgetTime.exe > c:\opt\bin\WinWidgetTime.bat
+echo start "" %dstDir%\winwidgets\WinWidgetTime.exe > %dstDir%\WinWidgetTime.bat
 
